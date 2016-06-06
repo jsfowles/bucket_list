bb2d3a1412ea5f0faef2a9ae0fb812c05535fed1
mghadley <matthew.g.hadley@gmail.com>
2016-06-06 14:04:48 -0600

    date formatting

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,11 @@
 module ItemsHelper
+
+	def display_date(item)
+		"#{item.complete_by.strftime("%B")} #{item.complete_by.day}, #{item.complete_by.year}"
+	end
+
+	def display_completed(item)
+		"#{item.completed_date.strftime("%B")} #{item.completed_date.day}, #{item.completed_date.year}"
+	end
+
 end
